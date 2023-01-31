9c5f12901fa0004535f5ad9e8b2ee8dcf5b0af7f
Marcos Paulo Marques Correa Gomes <t2m.mpgomes@terceiros.mag.com.br>
2023-01-31 19:48:24 -0300

    code: ajuste e add opcao for

--- mentalista/setup.js
+++ mentalista/setup.js
@@ -1,8 +1,9 @@
 var numeroSecreto = Math.round(Math.random() * 101)
 
 /*
+//OPCAO WHILE
 var count = 1
-while (palpite != numeroSecreto && count < 3) {    
+while (palpite != numeroSecreto && count <= 5) {    
     var palpite = prompt('Digite um nuemro de 1 a 100')
     if (palpite == numeroSecreto) {
         alert('Voce acertou, o numero é ' + numeroSecreto)        
@@ -17,15 +18,18 @@ while (palpite != numeroSecreto && count < 3) {
     }
 }
 */
-for (var i = 0; palpite != numeroSecreto && i < 3; i++) {
-    var palpite = prompt('Digite um nuemro de 1 a 100')
+//OPCAO FOR
+for (var i = 1; palpite != numeroSecreto && i <= 5; i++) {
+    var palpite = prompt('Digite um nuemro de 1 a 100, voce tem 5 chances')
     if (palpite == numeroSecreto) {
-        alert('Voce acertou, o numero é ' + numeroSecreto)        
+        alert('Voce acertou, o numero secreto é ' + numeroSecreto)        
     }
     else if (palpite > numeroSecreto)  {
         alert('O numero secreto  é menor que o palpite ' + i )        
     }
-    else if (palpite < numeroSecreto) {
+    else {
     alert('O numero secreto é maior que o palpite ' + i)       
     }
 }
+
+alert('Voce nao acertou, o numero secreto era o ' + numeroSecreto) 
