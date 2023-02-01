b10ed99f1b87283864d8c71c27ca7cc63990fb05
Marcos Paulo Marques Correa Gomes <t2m.mpgomes@terceiros.mag.com.br>
2023-02-01 13:51:58 -0300

    code: aluraflix

---
+++ aluraflix/setup.js
@@ -0,0 +1,24 @@
+
+var filmesList = [
+    "http://bancodeseries.com.br/images/posters/17372.jpg",
+    "https://ludopedio.org.br/wp-content/uploads/ted-lasso-682x1024.jpeg",
+    "https://classic.exame.com/wp-content/uploads/2023/01/Thats-90.jpg?quality=70&strip=info&w=574"
+]
+
+for (var index = 0; filmesList.length > index; index++) {
+    document.write("<img src=" + filmesList[index] + ">")
+}
+
+document.write("<br>")
+
+var count = 0
+while (filmesList.length > count) {
+    document.write("<img src=" + filmesList[count] + ">")
+    count++
+}
+
+document.write("<br>")
+
+filmesList.forEach(element => {
+    document.write("<img src=" + filmesList[length++] + ">")
+});
