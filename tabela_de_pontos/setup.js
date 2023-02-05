fbb3bd7f4f15993564c21b500ab44d69d1b6c1c2
Marcos Paulo Marques Correa Gomes <t2m.mpgomes@terceiros.mag.com.br>
2023-02-05 13:59:01 -0300

    code: pontos init

---
+++ tabela_de_pontos/setup.js
@@ -0,0 +1,34 @@
+var pauloObj = {
+    nome: 'Paulo',
+    vitoria: 0,
+    empate: 0,
+    derrota: 0,
+    pontos: 0
+}
+var rafaObj = {
+    nome: 'Paulo',
+    vitoria: 0,
+    empate: 0,
+    derrota: 0,
+    pontos: 0
+}
+var guiObj = {
+    nome: 'Paulo',
+    vitoria: 0,
+    empate: 0,
+    derrota: 0,
+    pontos: 0
+}
+
+
+function adicionarVitoria() {
+
+}
+
+function adicionarEmpate() {
+    
+}
+
+function adicionarDerrota() {
+    
+}
