1fba9da86751f5235f443fdb1c2aa824044ff1e9
Marcos Paulo Marques Correa Gomes <t2m.mpgomes@terceiros.mag.com.br>
2023-02-06 09:01:01 -0300

    code: update

--- tabela_de_pontos/setup.js
+++ tabela_de_pontos/setup.js
@@ -20,15 +20,38 @@ var guiObj = {
     pontos: 0
 }
 
+var elementoTabela = document.getElementById('tabelaJogadores')
 
-function adicionarVitoria() {
+exibirTela()
 
+function exibirTela() {
+    elementoTabela.innerHTML = `
+        <tr>
+            <td>${pauloObj.nome}</td>
+            <td>${pauloObj.vitoria}</td>
+            <td>${pauloObj.empate}</td>
+            <td>${pauloObj.derrota}</td>
+            <td>${pauloObj.pontos}</td>
+            <td><button onClick="adicionarVitoria(pauloObj)">Vitória</button></td>
+            <td><button onClick="adicionarEmpate(pauloObj)">Empate</button></td>
+            <td><button onClick="adicionarDerrota(pauloObj)">Derrota</button></td>
+        </tr>
+    `
 }
 
-function adicionarEmpate() {
-    
+function adicionarVitoria(jogador) {
+    jogador.vitoria++
+    jogador.pontos = jogador.pontos + 3
+    exibirTela()
 }
 
-function adicionarDerrota() {
-    
+function adicionarEmpate(jogador) {
+    jogador.empate++
+    jogador.pontos++
+    exibirTela()
+}
+
+function adicionarDerrota(jogador) {
+    jogador.derrota++
+    exibirTela()
 }
